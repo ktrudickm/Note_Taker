33d4ab0914c5de0f1624985289df6033707cf79e
Kendall <ktrudickm@gmail.com>
2021-04-05 17:56:00 -0400

    Added files and edited

---
+++ Develop/Routes/apiRoutes.js
@@ -0,0 +1,3 @@
+  
+const notesData = require('../data/notesData');
+
---
+++ Develop/data/notesData.js
@@ -0,0 +1,9 @@
+const notesArray = [
+    {
+      noteTitle: 'Note Title',
+      noteText: 'Note details and text',
+    },
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = notesArray;
