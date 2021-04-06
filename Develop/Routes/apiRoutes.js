f0ed3d0a2d158e03592c446a78f496368c6978b7
Kendall <ktrudickm@gmail.com>
2021-04-06 11:49:16 -0400

    Wrote apiROutes.js file including the app.get and app.post

--- Develop/Routes/apiRoutes.js
+++ Develop/Routes/apiRoutes.js
@@ -1,3 +1,17 @@
   
 const notesData = require('../data/notesData');
 
+module.exports = (app) => {
+
+    app.get('/api/notes', (req, res) => res.json(notesData));
+
+    app.post('/api/notes', (req, res) => {
+        notesData.push(req.body);
+    });
+
+    app.post('/api/clear', (req, res) => {
+        notesData.length = 0;
+
+        res.json({ ok: true });
+    });
+};
