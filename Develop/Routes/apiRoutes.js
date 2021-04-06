9c79dfa793e3b549243cef499b0e816a76914729
Kendall <ktrudickm@gmail.com>
2021-04-06 12:00:53 -0400

    Added to htmlRoutes.js file

--- Develop/Routes/apiRoutes.js
+++ Develop/Routes/apiRoutes.js
@@ -1,16 +1,16 @@
   
-const notesData = require('../data/notesData');
+const noteData = require('../data/notesData');
 
 module.exports = (app) => {
 
-    app.get('/api/notes', (req, res) => res.json(notesData));
+    app.get('/api/notes', (req, res) => res.json(noteData));
 
     app.post('/api/notes', (req, res) => {
-        notesData.push(req.body);
+        noteData.push(req.body);
     });
 
     app.post('/api/clear', (req, res) => {
-        notesData.length = 0;
+        noteData.length = 0;
 
         res.json({ ok: true });
     });
