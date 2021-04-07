dfa1c97675f5155d3ada563ccdb7397af1047a0e
Kendall <ktrudickm@gmail.com>
2021-04-06 22:04:31 -0400

    Edited server.js to include express dirname for public

--- Develop/server.js
+++ Develop/server.js
@@ -15,12 +15,13 @@ const PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + "/public"));
 
 // ROUTER
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-require("./Develop/Routes/apiRoutes")(app);
-require("./Develop/Routes/htmlRoutes")(app);
+require("./Routes/apiRoutes")(app);
+require("./Routes/htmlRoutes")(app);
 
 
 // LISTENER
